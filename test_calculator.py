562c8e13cb3638bf586739a8b522d054ea24a7f5
Tanakorn12345 <147680957+Tanakorn12345@users.noreply.github.com>
2025-11-10 05:11:37 +0000

    edit naja

--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,37 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1, 2), 3)
 
     # Add the following test methods to the TestCalculator class:
+    def test_addd(self):
+        self.assertEqual(self.calc.add(5, 2), 7)
 
+    def test_adddd(self):
+        self.assertEqual(self.calc.add(6 , 2) , 8 )
+    
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(5 , 4 ) , 1)
+    
+
+    def test_subb(self):
+        self.assertEqual(self.calc.subtract(6 , 2 ) , 4)
+    
+    def test_multi(self):
+        self.assertEqual(self.calc.multiply(5 , 5 ) , 25)
+    
+    def test_multii(self):
+        self.assertEqual(self.calc.multiply(6 , 5 ) , 30 )
+    
+    def test_divi(self):
+        self.assertEqual(self.calc.divide(4, 2 ) , 2 )
+    
+    def test_divii(self):
+        self.assertEqual(self.calc.divide(6 , 2 ) , 3)
+    
+    def test_modu(self):
+        self.assertEqual(self.calc.modulo(5 , 2 ) , 1)
+    
+    def test_moduu(self):
+        self.assertEqual(self.calc.modulo(18 , 9 ) , 0)
+
+        
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    
